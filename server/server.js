c8a04ad64e2077d50b3f3b65f0a1fb242f714885
Basel-shibob <shibobbasel@gmail.com>
2026-07-22 14:16:36 +0300

    Fix: add manual CORS headers middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,16 @@ const app = express();
 
 connectDB();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
